7889cfdbe74d99607a9a25338dc6d46ce44ad2e9
franciscogamezdg <franciscogamezdg@gmail.com>
2020-10-26 17:18:03 -0600

    Agrega dependencias y archivos de loging: * zipkin(sluth), logstash(JSON) * archivos logback-spring.xml para configuracion de logs con json

---
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
@@ -0,0 +1,15 @@
+package guru.sfg.beer.order.service.web.mappers;
+
+import javax.annotation.processing.Generated;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-10-26T15:49:46-0600",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.7 (Oracle Corporation)"
+)
+@Component
+@Primary
+public class BeerOrderLineMapperImpl extends BeerOrderLineMapperDecorator implements BeerOrderLineMapper {
+}
