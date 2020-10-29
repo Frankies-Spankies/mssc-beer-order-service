396d994d88f776e02883b3d3f73c99aac1a22b34
franciscogamezdg <franciscogamezdg@gmail.com>
2020-10-29 16:36:37 -0600

    Refactorizacion dependencias: 1.1 Actualiza BOM en order beer e inventory y elimina dependencias comunes

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
@@ -19,7 +19,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-10-26T15:49:47-0600",
+    date = "2020-10-29T15:55:35-0600",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.7 (Oracle Corporation)"
 )
 @Component
